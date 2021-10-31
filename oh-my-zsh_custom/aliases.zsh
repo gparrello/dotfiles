0c9cf6b202e1356939fb393e88f8c5879663fcab
Gerardo Parrello <gparrellobrasca@gmail.com>
2021-10-31 15:10:53 +0100

    add ohmyzsh custom

---
+++ oh-my-zsh_custom/aliases.zsh
@@ -0,0 +1,13 @@
+alias a="nano $HOME/.oh-my-zsh/custom/aliases.zsh"
+alias b="byobu"
+alias c="clear"
+alias ds="df -h"
+alias j="java -jar"
+alias n="nano"
+alias q="exit"
+alias z="nano $HOME/.zshrc"
+
+alias ¡="watch "
+
+alias cat="batcat"
+alias minecraft="java -jar $HOME/.minecraft/launcher.jar" # &> /dev/null &"
---
+++ oh-my-zsh_custom/pyenv.zsh
@@ -0,0 +1,4 @@
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+#export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+eval "$(pyenv init --path)"
