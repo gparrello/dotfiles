005bb7be9baf32f5f0e2e3e97779e5a5f1ec6513
Gerardo Parrello <gparrellobrasca@gmail.com>
2021-10-31 15:26:59 +0100

    add install ohmyzsh

--- installers/ohmyzsh.sh
+++ installers/ohmyzsh.sh
@@ -1,9 +1,9 @@
 sh <(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)
 
-(
-#cd $ZSH_CUSTOM/plugins
-cd $HOME/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone git@github.com:unixorn/fzf-zsh-plugin.git
-)
+#(
+##cd $ZSH_CUSTOM/plugins
+#cd $HOME/.oh-my-zsh/custom/plugins
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+#git clone git@github.com:unixorn/fzf-zsh-plugin.git
+#)
