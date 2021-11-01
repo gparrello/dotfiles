753402659cda24f2d8254def20a14eeb18b3e2c7
Gerardo Parrello <gparrellobrasca@gmail.com>
2021-11-01 23:51:11 +0100

    group k8s scripts

--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -1,3 +1,9 @@
+# minikube
+OUTPUT_FILE=/tmp/minikube.install
+wget -nv https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -O $OUTPUT_FILE
+sudo install $OUTPUT_FILE /usr/local/bin/minikube
+
+# kubectl
 sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main"\
