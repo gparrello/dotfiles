ac7337423b808e4afe574022f9e930b43c85573b
Gerardo Parrello <gparrellobrasca@gmail.com>
2021-11-02 00:10:16 +0100

    fix install

--- scripts/install_ohmyzsh.sh
+++ scripts/install_ohmyzsh.sh
@@ -1,3 +1,3 @@
 sudo apt install -y zsh
-wget -nv -O - https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-sudo usermod -s $(which zsh) $USER
+# sudo usermod -s $(which zsh) $USER
+sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
