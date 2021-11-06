4bc47caa83b254f94f50b1c4eee555d9b092c819
Gerardo Parrello <gparrellobrasca@gmail.com>
2021-11-06 21:55:48 +0100

    fix k8s install

--- scripts/install_atom.sh
+++ scripts/install_atom.sh
@@ -1,2 +1,4 @@
-wget -nv -O /tmp/atom.deb https://atom.io/download/deb
-sudo apt install -y /tmp/atom.deb
+OUTPUT_FILE=/tmp/atom.deb
+wget -nv -O $OUTPUT_FILE https://atom.io/download/deb
+sudo apt install -y $OUTPUT_FILE
+rm $OUTPUT_FILE
--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -9,7 +9,7 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main"\
   | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update && sudo apt install -y kubectl
-kubectl completion zsh > "$ZSH_CUSTOM/kubectl.zsh"
+# kubectl completion zsh > "$ZSH_CUSTOM/kubectl.zsh"
 
 # k9s
 #wget -O - https://github.com/derailed/k9s/releases/download/v0.24.15/k9s_Linux_x86_64.tar.gz | tar xvzf k9s -C ../bin/
