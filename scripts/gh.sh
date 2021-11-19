018f48d433ad2374069d0ac8d6cce272d30f9cec
Gerardo Parrello <gparrellobrasca@gmail.com>
2021-11-19 19:45:38 +0100

    add github cli install script

---
+++ scripts/gh.sh
@@ -0,0 +1,8 @@
+version="2.2.0"
+release="gh_$version"
+release+="_linux_amd64"
+wget -nv -O - "https://github.com/cli/cli/releases/download/v$version/$release.tar.gz"\
+	| tar xvzf - "$release"/bin/gh
+mv "$release/bin/gh" "$HOME/.local/bin/"
+rmdir "$release/bin"
+rmdir "$release"
