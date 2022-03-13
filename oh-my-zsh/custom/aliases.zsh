df43a20299fc237044f1ac0b4a22d3997339baef
Gerardo Parrello <gparrellobrasca@gmail.com>
2022-03-13 15:07:31 +0100

    add new aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
-alias a="nano $HOME/.oh-my-zsh/custom/aliases.zsh"
+alias a="nano $ZSH_CUSTOM/aliases.zsh"
 alias b="byobu"
 alias c="clear"
 alias ds="df -h"
@@ -11,3 +11,5 @@ alias ¡="watch "
 
 alias cat="batcat"
 alias minecraft="java -jar $HOME/.minecraft/launcher.jar" # &> /dev/null &"
+
+alias archive_gh_repos='while read -r r <&3; do echo -n "$r, "; read "yn?archive? "; case $yn in; [Yy]* ) gh repo archive "$r";; [Nn]* ) echo "nothin done";; [Qq]* ) echo "aborting"; break;; * ) echo "answer correctly dude";; esac; done 3< <(gh repo list --no-archived | cut -f 1)'
